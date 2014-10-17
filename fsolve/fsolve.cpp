eebe2ef5256593cf02cb1f6a715a8a1049c6cd96
Anne_ctwtm <j.a.dejong@utwente.nl>
2014-10-17 11:26:03 +0200

    Implemented boundary layers (logarithmic)

--- fsolve/fsolve.cpp
+++ fsolve/fsolve.cpp
@@ -1,9 +1,9 @@
 #include "fsolve.h"
-
+#include <math.h>
 
 namespace math_common{
 
-  vd fsolve(vd& guess,vdfun f){
+  vd fsolve(const vd& guess,vdfun& f){
     TRACE(0,"fsolve ");
     //Newton-Raphson implementation
     vd x=guess;
@@ -20,14 +20,14 @@ namespace math_common{
     do {
       fx=f(x);
       if(norm(x,2)<1e-8)
-	deltax=1e-9;
+        deltax=1e-9;
       else
-	deltax=1e-3*norm(x,2);
+        deltax=1e-3*norm(x,2);
       for(i=0;i<size;i++){
-	x2=x;
-	x2(i)+=deltax;
-	fx2=f(x2);
-	dfdx.col(i)=(fx2-fx)/deltax;      
+        x2=x;
+        x2(i)+=deltax;
+        fx2=f(x2);
+        dfdx.col(i)=(fx2-fx)/deltax;      
       }
 
       dx=-1.0*arma::solve(dfdx,fx);
@@ -36,6 +36,37 @@ namespace math_common{
       funerror=norm(fx,2);
       nloop++;
     } while(funerror>FUNTOL || relerror>RELTOL);
+    return x;
+  }
+  d Fsolverd::operator()(dfun& f,const d& guess){
+    TRACE(10,"Fsolverd::operator()");
+    //Newton-Raphson implementation
+    d x=guess;
+    d funerror,relerror,deltax,dfdx,fx,fx2,x2,dx;
+    us nloop=0;
+    do {
+      fx=f(x);
+      if(abs(x)<1e-8)
+        deltax=1e-9;
+      else
+        deltax=1e-3*abs(x);
+      x2=x+deltax;
+      fx2=f(x2);
+      dfdx=(fx2-fx)/deltax;      
+      dx=-1.0*fx/dfdx;
+      x+=dx;
+      relerror=std::abs(dx);
+      funerror=std::abs(fx);
+      nloop++;
+      if(verbose)
+        cout << std::scientific << "Iteration: " << nloop << ", funerror: " << funerror << " , relerror: " << relerror << "\n";
+    } while((funerror>funtol || relerror>reltol) && nloop<maxiter);
+
+    if(nloop==maxiter)
+      WARN("Solver done, but maxiter ("<<maxiter<<") reached. Results might be unreliable!");
+    if(verbose)
+      cout << "Solver done. Iterations: "<< nloop <<"\n";
+
     return x;
   }
 
--- fsolve/fsolve.h
+++ fsolve/fsolve.h
@@ -24,14 +24,34 @@
 #define MAXITER (1000)
 #endif
 using namespace std::placeholders;    /* adds visibility of _1, _2, _3,...*/
+
 namespace math_common{
   SPOILNAMESPACE
   using namespace std::placeholders;    /* adds visibility of _1, _2, _3,...*/
   // Not using boost anymore
+  
   typedef std::function<vd (const vd& x)> vdfun;
-  typedef std::function<dmat (const vd& x)> dmatfun;  
-  vd fsolve(vd& guess,vdfun f);
 
+  typedef std::function<dmat (const vd& x)> dmatfun;  
+  vd fsolve(const vd& guess,vdfun& f);
+
+  typedef std::function<d (const d& x)> dfun;  
+  class Fsolverd{
+    d funtol;
+    d reltol;
+    us maxiter;
+    bool verbose;
+  public:
+    Fsolverd(d funtol=1e-6,d reltol=1e-6,us maxiter=100,bool verbose=false):
+      funtol(funtol),
+      reltol(reltol),
+      maxiter(maxiter),
+      verbose(verbose)
+    {}
+    d operator()(dfun& f,const d& guess);
+    ~Fsolverd(){}
+    void setVerbose(bool verbose){this->verbose=verbose;}
+  };
   
 } // namespace math_common
   
