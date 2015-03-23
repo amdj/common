0cb777cacb3e58ddb112bd5dbd09121393fbfb1a
J.A. de Jong @ vulgaris <anne@amdj.nl>
2015-03-23 17:55:44 +0100

    Working getValue()

--- exception/exception.h
+++ exception/exception.h
@@ -1,6 +1,23 @@
+// exception.h
+//
+// Author: J.A. de Jong 
+//
+// Description:
+// My exception class
+//////////////////////////////////////////////////////////////////////
+#pragma once
+#ifndef EXCEPTION_H
+#define EXCEPTION_H 1
+
 #include <stdexcept>
 
 class MyError : public std::runtime_error {
 public:
   MyError(const string& msg = "") : runtime_error(msg) {}
 };
+
+
+
+#endif // EXCEPTION_H
+//////////////////////////////////////////////////////////////////////
+
