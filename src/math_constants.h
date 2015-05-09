83d4c9d920b549c41f92bc4343193498eec1e0f6
J.A. de Jong @ vulgaris <anne@amdj.nl>
2015-05-09 11:40:52 +0200

    Need sq2 to be double

--- src/math_constants.h
+++ src/math_constants.h
@@ -15,7 +15,7 @@ namespace math_common {
   SPOILNAMESPACE
   // These constants are so often used I implement them standardly
   const c sqI=sqrt(I);
-  const c sq2=sqrt(2.0);
+  const d sq2=sqrt(2.0);
   const c sqmI=sqrt(-1.0*I);
 
 } // namespace math_common
